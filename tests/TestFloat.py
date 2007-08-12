992d367ff29d266809c0c2fd55c716b7c6ecc706
Tim Ansell <mithro@mithis.com>
2007-08-13 02:37:16 +0930

    Added correct 'NaN' test.

--- tests/TestFloat.py
+++ tests/TestFloat.py
@@ -29,15 +29,32 @@ class TestFloat(object):
 		assert scm.type() == float
 		assert scm.topython() == value
 
+	def test_passthru_nan(self):
+		print "passthru_nan"
+
+		value = float('NaN')
+
+		m1 = Inter()
+		scm = m1.to_scheme(value)
+	
+		assert scm.type() == float
+		# float('NaN') != float('NaN')
+		assert str(scm.topython()) == 'nan'
+
 	def test_floats(self):
 		floats = {
 			'1.0':1.0, 
 			'0.0':0.0, 
+			# Complex types with zero imaginary part should come back as floats
+			'0.0+0.0i': 0.0,
+			'123.0+0.0i': 123.0,
 			# These don't seem to be portable :/
-			'`+inf.0':float('inf'), 
-			'`-inf.0':float('-inf'), 
-			'`+nan.0':float('NaN')}
+			'+inf.0':float('inf'), 
+			'-inf.0':float('-inf'), 
+		}
 
 		for s, value in floats.items():
 			yield self.eval_test, s, value
 			yield self.passthru_test, s, value
+
+
