cdf05cb55792b42639a1c9c8564fb60a95628833
pluskid <pluskid@gmail.com>
2008-04-27 12:06:26 -0400

    Added exception skeleton for schemepy.

---
+++ schemepy/exceptions.py
@@ -0,0 +1,21 @@
+# Exception hierarchy for Schemepy
+
+class Error(Exception):
+    "The base class for the Exceptions in Schemepy."
+    pass
+
+
+class ConversionError(Error):
+    """\
+    Exception raised if a conversion can't be done properly.
+
+    Attributes:
+        value   - the value to be converted
+        message - the error message
+    """
+    
+    def __init__(self, value, message):
+        self.value = value
+        self.message = message
+        
+    
