7b7ed882e048e90a45f15712272302e72a6e9855
pluskid <pluskid@gmail.com>
2008-05-13 15:26:36 -0400

    Let the test suites of schemepy use the schemepy VM interface.

--- tests/schemepy/common.py
+++ tests/schemepy/common.py
@@ -1,15 +1,6 @@
 import os
-from schemepy import types
+import schemepy
+import schemepy.types
 
-try:
-    backend = os.environ['BACKEND']
-except KeyError:
-    raise Exception, "You need to specify the BACKEND environment variable."
-
-try:
-    exec("from schemepy.%s import %s as backend" % (backend, backend))
-except ImportError:
-    raise Exception("Backend named %s doesn't exist." % backend)
-
-VM = backend.VM
-compiler = backend.Compiler
+types = schemepy.types
+VM = schemepy.VM
