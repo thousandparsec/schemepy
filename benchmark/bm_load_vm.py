4a3f1dd1dea07ca533a53f30fa96867de45df3ee
pluskid <pluskid@gmail.com>
2008-05-16 16:42:17 -0400

    Added benchmark for loading VM.

---
+++ benchmark/bm_load_vm.py
@@ -0,0 +1,21 @@
+"""\
+This benchmark test the loading time of each backend.
+
+But it seems to be hard to make this benchmark accurate. Because
+it is only slow for the first time when a VM is to be loaded. Later
+it will be very fast since all related stuffs are already in memory.
+So repeating here just makes no help.
+"""
+
+from benchmark import Benchmark
+import helper
+
+def load_backend(backend):
+    vm = helper.VM(backend=backend)
+
+bm = Benchmark(title="Time to load the VM", repeat=1)
+
+for backend in helper.BACKENDS:
+    bm.measure(load_backend, backend, title=backend)
+
+helper.report(bm.report())
---
+++ benchmark/helper.py
@@ -0,0 +1,33 @@
+import sys
+import os.path
+
+sys.path.append(
+    os.path.join(os.path.dirname(__file__), ".."))
+
+from schemepy import VM
+from benchmark import Benchmark
+
+
+def read(file_name):
+    io = open(file_name)
+    cont = io.read()
+    io.close()
+    return cont
+
+
+BACKENDS = ['pyscheme', 'guile']
+
+def load_backends():
+    """\
+    Force the backends to be loaded. The loading time
+    of the backends will be benchmarked separately.
+    """
+    for backend in BACKENDS:
+        schemepy.VM(backend=backend)
+
+def report(report):
+    """\
+    Show the report.
+    """
+    print report
+    
